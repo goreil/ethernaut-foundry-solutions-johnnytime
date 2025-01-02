0ad475258b638e40b7370881221f8698f49f33c8
ylue-study <>
2025-01-02 17:51:40 +0100

    Fix "src/GatekeeperOne.sol" not found:

--- script/GateKeeperOneSolution.s.sol
+++ script/GateKeeperOneSolution.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
 import "forge-std/console.sol";
-import {GatekeeperOne} from "../src/GatekeeperOne.sol";
+import {GatekeeperOne} from "../src/GateKeeperOne.sol";
  
 contract GatekeeperOneSolution is Script {
 
